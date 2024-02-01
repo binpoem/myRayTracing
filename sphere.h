0f7c01de740e2a41a3f3efcfd65ce9cc35c42390
binpoem <binpoem@gmail.com>
2024-02-01 14:11:35 +0800

    CH2 Motion Blur

--- sphere.h
+++ sphere.h
@@ -7,12 +7,21 @@
 class sphere : public hittable
 {
 public:
+  // Stationary Sphere
   sphere(point3 _center, double _radius, shared_ptr<material> _material)
-    : center(_center), radius(_radius), mat(_material)
+    : center1(_center), radius(_radius), mat(_material), is_moving(false)
   {}
+
+  // Moving Sphere
+  sphere(point3 _center1, point3 _center2, double _radius, shared_ptr<material> _material)
+    : center1(_center1), radius(_radius), mat(_material), is_moving(true)
+  {
+    center_vec = _center2 - _center1;
+  }
 
   bool hit(const ray &r, interval ray_t, hit_record &rec) const override
   {
+    point3 center = is_moving ? sphere_center(r.time()) : center1;
     vec3 oc = r.origin() - center;
     auto a = r.direction().length_squared();
     auto half_b = dot(oc, r.direction());
@@ -39,9 +48,18 @@ public:
   }
 
 private:
-  point3 center;
+  point3 center1;
   double radius;
   shared_ptr<material> mat;
+  bool is_moving;
+  vec3 center_vec;
+
+  point3 sphere_center(double time) const
+  {
+    // Linearly interpolate from center1 to center2 according to time, where t=0 yields
+    // center1, and t=1 yields center2.
+    return center1 + time * center_vec;
+  }
 };
 
 #endif
